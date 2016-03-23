44698f07aed97da4bf8a93d302a1bc0b58cb9bcc
adv <a@i.it>
2016-03-23 12:09:48 +0100

    Restful API

--- src/main/java/masterSpringMvc/config/WebConfiguration.java
+++ src/main/java/masterSpringMvc/config/WebConfiguration.java
@@ -6,7 +6,9 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,9 +18,17 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.util.UrlPathHelper;
 
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import java.time.LocalDate;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
@@ -54,5 +64,21 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 		UrlPathHelper urlPathHelper = new UrlPathHelper();
 		urlPathHelper.setRemoveSemicolonContent(false);
 		configurer.setUrlPathHelper(urlPathHelper);
+		configurer.setUseRegisteredSuffixPatternMatch(true);
+	}
+
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+		ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+				false);
+		return objectMapper;
+	}
+
+	@Bean
+	public Docket userApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.paths(path -> path.startsWith("/api/")).build();
 	}
 }
